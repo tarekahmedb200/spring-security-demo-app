1dbef1b20761f2b8e51ebf1fea13b04135fd5b15
Admin <admin@lapshops-MacBook-Pro.local>
2026-01-11 10:49:56 +0200

    using in memory cache to get users

--- src/main/java/com/example/spring_secuurity_demo/student/security/SecurityConfig.java
+++ src/main/java/com/example/spring_secuurity_demo/student/security/SecurityConfig.java
@@ -1,16 +1,31 @@
 package com.example.spring_secuurity_demo.student.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,5 +41,23 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails anna = User.builder()
+                .username("anna")
+                .password(passwordEncoder.encode("anna"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                anna
+        );
+    }
+
+
+
+
 
 }
