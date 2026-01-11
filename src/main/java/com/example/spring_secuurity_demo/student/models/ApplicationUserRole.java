d146316ff2b2930687f707c9032a02462f23b217
Admin <admin@lapshops-MacBook-Pro.local>
2026-01-11 11:35:24 +0200

    adding roles and permissions and filter based on role

---
+++ src/main/java/com/example/spring_secuurity_demo/student/models/ApplicationUserRole.java
@@ -0,0 +1,28 @@
+package com.example.spring_secuurity_demo.student.models;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.example.spring_secuurity_demo.student.models.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(
+            COURSE_READ,
+            COURSE_WRITE,
+            STUDENT_READ,
+            STUDENT_WRITE
+            )),
+    ADMINTRAINEE(Sets.newHashSet(
+            COURSE_READ,
+            STUDENT_READ
+          ));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+}
--- src/main/java/com/example/spring_secuurity_demo/student/security/SecurityConfig.java
+++ src/main/java/com/example/spring_secuurity_demo/student/security/SecurityConfig.java
@@ -1,19 +1,21 @@
 package com.example.spring_secuurity_demo.student.security;
 
 
+import com.example.spring_secuurity_demo.student.models.ApplicationUserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static com.example.spring_secuurity_demo.student.models.ApplicationUserRole.*;
 
 @Configuration
 public class SecurityConfig {
@@ -33,7 +35,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         // Only GET /students is public
-                        .requestMatchers(HttpMethod.GET, "/api/v1/students").permitAll()
+                        .requestMatchers("/api/**").hasRole(STUDENT.name())
+                        .requestMatchers("/api/**").hasRole(STUDENT.name())
                         // All other requests require authentication
                         .anyRequest().authenticated()
                 )
@@ -48,15 +51,27 @@ public class SecurityConfig {
         UserDetails anna = User.builder()
                 .username("anna")
                 .password(passwordEncoder.encode("anna"))
-                .roles("STUDENT")
+                .roles(STUDENT.name())
+                .build();
+
+        UserDetails angel = User.builder()
+                .username("angel")
+                .password(passwordEncoder.encode("angel"))
+                .roles(ADMIN.name())
+                .build();
+
+        UserDetails tom = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("tom"))
+                .roles(ADMINTRAINEE.name())
                 .build();
 
         return new InMemoryUserDetailsManager(
-                anna
+                anna,
+                angel,
+                tom
         );
     }
-
-
 
 
 
